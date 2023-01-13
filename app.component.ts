db554c6187684581bcf9dc10e21e70dda4bc0414
Utkrisht Trivedi <trivedirocker007@gmail.com>
2023-01-13 12:04:11 +0530

    Add files via upload
    App with map click and table display functionality
    
    Signed-off-by: Utkrisht Trivedi <trivedirocker007@gmail.com>

--- app.component.ts
+++ app.component.ts
@@ -1,72 +1,112 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { FormGroup, FormArray, FormControl, FormBuilder} from '@angular/forms';
-import * as L from 'leaflet'; 
-import { indexOf } from 'underscore';
-  import {Chart} from 'chart.js';
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import TileWMS from 'ol/source/TileWMS.js';
+import * as $ from "jquery";
+import Chart from 'chart.js/auto';
+import  {transform}  from 'ol/proj';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 
-export class AppComponent implements AfterViewInit {
-  private map1;
-  private map2;
-  private map3;
-  private map4;
-  private wmsLayer1;
-  private wmsLayer2;
-  private wmsLayer3;
-  private initMap(): void {
-    this.wmsLayer1 = L.tileLayer.wms('http://190.168.51.85:8085/geoserver/HSVP/wms?', {
-    layers: 'HSVP:Buidings',
-    format: 'image/png',
-    transparent: true
-    }).addTo(this.map4);
-    var markar = L.marker([51.5, -0.09]).addTo(this.map4);  
-  }
+export class AppComponent implements OnInit {
 
-private initMap2(): void  {
-    this.wmsLayer2 =   L.tileLayer.wms('http://190.168.51.85:8085/geoserver/HSVP/wms?', {
-     layers: 'HSVP:Roads',
-     format: 'image/png',
-    transparent: true
-     }).addTo(this.map4);
-    var markar = L.marker([51.5, -0.09]).addTo(this.map4);       
-}
+  map: Map = new Map;
+  dtOptions: DataTables.Settings = {};
 
-private initMap3(): void {
-    this.wmsLayer3 =  L.tileLayer.wms('http://190.168.51.85:8085/geoserver/HSVP/wms?', {
-    layers: 'HSVP:Plot_Parcel',
-    format: 'image/png',
-    transparent: true
-    }).addTo(this.map4);
-    var markar = L.marker([51.5, -0.09]).addTo(this.map4);       
-}
-private initMap4(): void {
-    this.map4 = L.map('map', {
-    center: [28.3654, 77.2965],
-    zoom: 12
+  ngOnInit(): void {
+    /* this.map = new Map({
+      view: new View({
+        center: [0, 0],
+        zoom: 1,
+      }),
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: 'ol-map'
     });
-    const wmsLayer = L.tileLayer.wms('http://ows.mundialis.de/services/service?', {
-    layers: 'TOPO-OSM-WMS',
-    format: 'image/png',
-transparent: true
-    }).addTo(this.map4);
-    var markar = L.marker([51.5, -0.09]).addTo(this.map4);  
+*/
+    this.createChart();
+    this.dtOptions = {
+      paging: false,
+      scrollY: 150,
+      scrollX: true
+    };
+
+    const wmsSource = new TileWMS({
+      url: 'https://ahocevar.com/geoserver/wms',
+      params: {'LAYERS': 'ne:ne', 'TILED': true},
+      serverType: 'geoserver',
+      crossOrigin: 'anonymous',
+    });
+
+    const wmsLayer = new TileLayer({
+      source: wmsSource
+    });
+
+    const wmsSourceHSVPPlotParcel = new TileWMS({
+      url: 'http://190.168.51.85:8085/geoserver/HSVP/wms',
+      params: {'LAYERS': 'HSVP:Plot_Parcel', 'TILED': false},
+      serverType: 'geoserver',
+      crossOrigin: 'anonymous',
+    });
+
+    const wmsLayerHSVPPlotParcel = new TileLayer({
+      source: wmsSourceHSVPPlotParcel
+    });
+
+    const view = new View({
+      center:transform([77.2965,28.3654],"EPSG:4326","EPSG:3857"),
+      // center: [77.2965,28.3654],
+      zoom: 11,
+    });
+        
+    const map = new Map({
+      layers: [wmsLayer],
+      target: 'map',
+      view: view,
+    });
+        
+    map.addLayer(wmsLayerHSVPPlotParcel);
+
+    
+
+    map.on('singleclick', function (evt) {
+      (<HTMLInputElement>document.getElementById('info')).innerHTML = '';
+      const data = wmsLayer.getData(evt.pixel);
+      const viewResolution = view.getResolution() as number;
+
+      console.log(viewResolution);
+      const url = wmsSource.getFeatureInfoUrl(
+        evt.coordinate,
+        viewResolution,
+        'EPSG:3857',
+        {'INFO_FORMAT': 'text/html'}
+      );
+      if (url) {
+        fetch(url)
+          .then((response) => response.text())
+          .then((html) => {
+            (<HTMLInputElement>document.getElementById('info')).innerHTML = html;
+          });
+      }
+    });
+  
   }
 
-  constructor(){}
-
-  ngAfterViewInit(): void {
-      
-  }
-
-  title = 'htmlone';
-
-  public map;
   public chart: any;
+
+  ReadMore:boolean = true
+  visible:boolean = false
+
+  visible1:boolean = false
 
   public show:boolean = false;
   public buttonName:any = 'Show';
@@ -74,82 +114,53 @@ transparent: true
   options = [ 
     "Option 1",
     "Option 2",
-    "Option 3",
-    "Option 4"
-    ];
+    "Option 3"
+  ];
 
-    optionsArray: Array<any> = [];
+  optionsArray: Array<any> = [];
   
-
-
   onChange(op:string, $target: EventTarget) {
+
     const ischecked = (<HTMLInputElement>$target).checked;
-
-      var index = this.options.indexOf(op);
-      /*this.optionsArray.push(op);*/
-      
-      if(index == 0){
-        if(ischecked) {
-          this.initMap();
-        }
-        else{
-          this.map4.removeLayer(this.wmsLayer1);
-        }
+    var index = this.options.indexOf(op);
+    this.optionsArray.push(op);
+    var index = this.options.indexOf(op);
+    if(index == 0){
+      if(ischecked){
+        this.visible1 = !this.visible1;
       }
-      else if(index == 1){
-        if(ischecked) {
-          this.initMap2();
-        }
-        else{
-          this.map4.removeLayer(this.wmsLayer2);
-        }
+      else{
+        this.visible1 = !this.visible1;
       }
-      else if(index == 2){
-        if(ischecked) {
-          this.initMap3();
-        }
-        else{
-          this.map4.removeLayer(this.wmsLayer3);
-        }
-      }
-      else if(index == 3){
-        if(ischecked) {
-          this.initMap4();
-        }
-        else{
-          this.map4.remove();
-        }
-      }
+    }
   }
 
-  createChart(){
-  
-    this.chart = new Chart("MyChart", {
-      type: 'bar', //this denotes tha type of chart
+  onclick(op:string, $target: EventTarget) {
+    this.visible = !this.visible;
+    this.ReadMore = !this.ReadMore;
+  }
 
-      data: {// values on X-Axis
-        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
-								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
-	       datasets: [
-          {
-            label: "Sales",
-            data: ['467','576', '572', '79', '92',
-								 '574', '573', '576'],
-            backgroundColor: 'blue'
+    createChart(){
+      var xValues = ["GIS_Plot_S", "Vacant", "Allotted", "Encroachment", "Open Space"];
+      var yValues = [55, 49, 44, 24, 15];
+      var barColors = ["red", "green","blue","orange","brown"];
+      this.chart = new Chart("MyChart",  {
+        type: "bar",
+        data: {
+          labels: xValues,
+          datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+          }]
+        },
+        options: {
+          plugins: {
+            legend: {
+              display: false,
+            }
           },
-          {
-            label: "Profit",
-            data: ['542', '542', '536', '327', '17',
-									 '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
-          }  
-        ]
-      },
-      options: {
-        aspectRatio:2.5
-      }
-      
-    });
-  }
-
+          aspectRatio:1.1
+        }
+      });
+    }
 }
